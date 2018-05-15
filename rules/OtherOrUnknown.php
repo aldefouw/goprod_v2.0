a082f53dade891ae73c9662ba3bbaa35a99c6c60
aandresalvarez <aandresalvarez@gmail.edu>
2018-05-15 11:58:01 -0700

    testing

--- rules/OtherOrUnknown.php
+++ rules/OtherOrUnknown.php
@@ -18,17 +18,14 @@ function OtherOrUnknown(){
 
 
     $phat_to_rule= dirname(dirname(__FILE__)) . '/classes/Check_other_or_unknown.php';
-
     if(!@include_once($phat_to_rule)){ error_log("Failed to include:: $phat_to_rule");}
-
     $res= new check_other_or_unknown();
     $Rule['results']=$res::CheckOtherOrUnknown();
-   // error_log( print_r($Rule, TRUE));
+    // error_log( print_r($Rule, TRUE));
     //if problems not found
     if(empty($Rule['results'])){$Rule['results']=false;}
     return $Rule;
 
 }
-
 
 //tiene que retornar en $Rule['results'] un array con datos o vacio si es positivo  o false si no se encotro el problema
--- views/skip_form.php
+++ views/skip_form.php
@@ -10,35 +10,33 @@
 <form class="form-horizontal">
     <fieldset>
 
-        <!-- Form Name -->
-        <legend>Form Name</legend>
+
 
         <!-- Textarea -->
-        <div class="form-group">
-            <table><tr>
+        <div class="form-group" style="padding: 5%">
+            <table   >
+                <tr>
+
                     <td>
-                        <label class="col-md-11 control-label" for="textarea">
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                            It has survived not only five centuries, but also the leap into electronic typesetting, remaining
-                            essentially unchanged.
+                        <label for="textarea">
+                            Use this option if you consider this is not an issue or if the remain issues are not a problem for your REDCap Database.
                         </label>
                     </td>
                 </tr>
                 <tr>
-                    <td> <div class="col-md-11">
+                    <td>
                             <textarea class="form-control" id="textarea" name="textarea">default text</textarea>
-                        </div></td>
+                        </td>
                 </tr>
-                <tr><td>
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining
-                        essentially unchanged.
+                <tr>
+                    <td>
+                        <p>
+                            Please note that future problems with omitting this recommendation may result in additional support costs for your project.
+                        </p>
                     </td>
                 </tr>
                 <tr><td>
-                        <div class="col-md-11">
+                        <div  >
                             <label class="checkbox-inline" for="checkboxes-0">
                                 <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
                                 1
