576dc5a90e3f6fc93ac0d20bb9caadce87ba7ad9
alanisem123 <45210416+alanisem123@users.noreply.github.com>
2018-12-03 22:20:51 -0500

    RxOrQI.php
    Added rule that triggers the class to check whether a project is either Research or Quality Improvement. The file makes reference to the corresponding message-language file along to its corresponding function found within the class of check_pi_irb.php.

---
+++ rules/RxOrQI.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alvaro1
+ * Date: 4/20/18
+ * Time: 1:52 PM
+ */
+
+
+namespace Stanford\GoProd;
+
+/* EM@partners.org*/
+
+
+function RxOrQI(){
+
+    $Rule['title']=lang('NOT_RX_OR_QI_TITLE');
+    $Rule['body']=lang('NOT_RX_OR_QI_BODY');
+    $Rule['risk']="danger"; // level of risk: warning, danger or info.
+
+    $phat_to_rule= dirname(dirname(__FILE__)) . '/classes/Check_pi_irb_type.php';
+
+    if(!@include_once($phat_to_rule)){ error_log("Failed to include:: $phat_to_rule");}
+    $res= new check_pi_irb_type();
+    $Rule['results']=$res::IsRxOrQI();
+    return  $Rule;
+}
