99eeaf1085d304f8ba86a5614702de5530b767e5
aandresalvarez <aandresalvarez@gmail.edu>
2018-12-04 09:29:03 -0800

    cleaning

---
+++ views/final_message.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alvaro1
+ * Date: 2018-12-03
+ * Time: 15:33
+ */
+
+
+
+
+
+
+
+?>
+
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Button</a>
+    </div>
+</div>
+<div class="card text-center">
+    <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
