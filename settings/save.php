0469fcf3ec48fad4d77cd68ff678dc475bfec893
Adam De Fouw <aldefouw@medicine.wisc.edu>
2021-03-19 14:40:34 -0500

    More refactoring

--- settings/save.php
+++ settings/save.php
@@ -13,25 +13,20 @@ if ( !$_SERVER['REQUEST_METHOD'] == 'POST'  ) {
 $file = $GLOBALS['modulePath'].'/settings/config.json';
 
 if(file_exists($file)){
-
-    $contents = file_get_contents($file, TRUE);
+    //Decode the file contents into JSON
     $json = json_decode(file_get_contents($file),TRUE);
 
     //Abstract all the fields into succinct foreach loop
     //This way whenever we add a new field, as long as we follow the naming convention, changes are applied
-    foreach($_POST as $key => $value){
-        foreach($_POST[$key] as $k => $v){
-            if(isset($_POST[$key][$k])) {
+    foreach($json as $key => $value){
+        foreach($json[$key] as $k => $v){
+            if($k == 'active') {
+                $json[$key][$k] = isset($_POST[$key][$k]) ? true: false;
+            } else if (isset($_POST[$key][$k])) {
                 $json[$key][$k] = trim($_POST[$key][$k]);
             }
         }
     }
-    
-    //If this is selected as active, check here
-    $json['other_or_unknown']['active'] = (isset($_POST["other-active-selected"])) ? true : false;
-    $json['yes_no']['active'] = (isset($_POST["yes-no-active-selected"])) ? true : false;
-    $json['test_records']['active'] = (isset($_POST["test-records-active-selected"])) ? true : false;
-    $json['positive_negative']['active'] = (isset($_POST["positive-negative-active-selected"])) ? true : false;
 
     //Write this JSON to a file
     file_put_contents($file, json_encode($json));
