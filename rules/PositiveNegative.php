f4f71360021ae6435e953b95ccc546c0a2a89c16
Adam De Fouw <aldefouw@medicine.wisc.edu>
2021-05-13 15:43:59 -0500

    Update PositiveNegative

--- rules/PositiveNegative.php
+++ rules/PositiveNegative.php
@@ -12,9 +12,9 @@ function PositiveNegative(){
     global $config_json;
 
     $Rule['configured_name'] = 'positive_negative';
-    $Rule['title']=lang('YES_NO_TITLE');
-    $Rule['body']=lang('YES_NO_BODY');
-    $Rule['risk']=$config_json['branching_logic']['type']; // level of risk: warning, danger or info.
+    $Rule['title']=lang('POSITIVE_NEGATIVE_TITLE');
+    $Rule['body']=lang('POSITIVE_NEGATIVE_BODY');
+    $Rule['risk']=$config_json['positive_negative']['type']; // level of risk: warning, danger or info.
 
     $phat_to_rule= dirname(dirname(__FILE__)) . '/classes/Check_consistency_for_lists.php';
 
